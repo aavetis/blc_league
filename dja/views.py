bce06e5369e151b42d5e4ce4459ddb86778f45c3
A A <bertmaistro1@gmail.com>
2014-04-21 18:09:12 -0400

    4/21 update.02

--- dja/views.py
+++ dja/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
